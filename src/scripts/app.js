b50fb4eb737348878d1403811dc9addc566f3f42
SudevKiyada <sudev.kiyada@gmail.com>
2022-06-30 11:44:19 +0530

    first draft

---
+++ src/scripts/app.js
@@ -0,0 +1,10 @@
+// Import all plugins
+import * as bootstrap from 'bootstrap';
+
+import { animate } from "motion";
+
+// animate(
+//     "#box",
+//     { background: "red" },
+//     { duration: 0.5 }
+//   );
